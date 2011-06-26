38a3c686c6e8f1d30a47207a845e05dae67c537e
Brian Dewey <bdewey@gmail.com>
2011-06-26 07:12:06 -0700

    Testing that keys are removed by filtering IPFlickrApiKeys.h

---
+++ Classes/IPFlickrApiKeys.h
@@ -0,0 +1,32 @@
+//
+//  IPFlickrApiKeys.h
+//
+//  This is where you define the API key and shared secret for interacting
+//  with Flickr. If I set up git correctly, these keys will get removed on
+//  checkin. If you work on your own version of this code, you will need to
+//  get your own Flickr API keys.
+//
+//  Created by Brian Dewey on 6/26/11.
+//  Copyright 2011 Brian Dewey. 
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+//#define PHOLIO_FLICKR_API_KEY ...
+//#define PHOLIO_FLICKR_API_SHARED_SECRET ...
+
+#ifndef PHOLIO_FLICKR_API_KEY
+#error You need to define an API key and shared secret to interact with Flickr.
+#endif
+
